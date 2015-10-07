32b3b0fcafeabc72840a6ac6947341559ebb361a
Michael Ekins <mekins86@gmail.com>
2015-10-07 21:17:40 +0100

    Added new validator for field length

---
+++ ConsoleApplication1/ValidatorFieldLength.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileValidator
+{
+    class ValidatorFieldLength : IValidator
+    {
+        private int fieldLength;
+
+        public ValidatorFieldLength(int aFieldLength)
+        {
+            fieldLength = aFieldLength;
+            
+        }
+
+        public Boolean ValidateField(string fieldText, string errorText)
+        {
+            if(fieldText.Length == fieldLength){
+                return true;
+            }
+            else{
+                errorText = "Field is not of length " + fieldLength.ToString() + ": " + fieldText;
+                return false;
+            }
+        }
+    }
+}
