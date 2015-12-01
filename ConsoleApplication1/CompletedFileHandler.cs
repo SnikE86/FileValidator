abc5b9095f6c6e8c181354d630434b9ca7c05527
Michael Ekins <mekins86@gmail.com>
2015-12-01 22:00:31 +0000

    Fixed bug with moving file due to paths

--- ConsoleApplication1/CompletedFileHandler.cs
+++ ConsoleApplication1/CompletedFileHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,22 +20,22 @@ namespace FileValidator
 
         public void MoveFileToSuccessDirectory(string fileName)
         {
-            if (!System.IO.Directory.Exists(SuccessDirectory))
-            {
-                System.IO.Directory.CreateDirectory(SuccessDirectory);
-            }
-
-            System.IO.File.Move(fileName, SuccessDirectory);
+            MoveFile(fileName, SuccessDirectory);
         }
 
         public void MoveFileToFailureDirectory(string fileName)
         {
-            if (!System.IO.Directory.Exists(FailureDirectory))
+            MoveFile(fileName, FailureDirectory);
+        }
+
+        private void MoveFile(string srcFileName, string dstPath)
+        {
+            if (!System.IO.Directory.Exists(dstPath))
             {
-                System.IO.Directory.CreateDirectory(FailureDirectory);
+                System.IO.Directory.CreateDirectory(dstPath);
             }
 
-            System.IO.File.Move(fileName, FailureDirectory);
+            System.IO.File.Move(srcFileName, Path.Combine(dstPath, Path.GetFileName(srcFileName)));
         }
     }
 }
